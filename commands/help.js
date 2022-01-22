0c2d493785fe14eb4b1a8a09cd9d93b3b4bc4147
Abhinav <csabhi108@gmail.com>
2022-01-22 23:36:20 +0530

    first commit

---
+++ commands/help.js
@@ -0,0 +1,11 @@
+function helpfn() {
+    console.log(`List of all the Commands-
+                      1) Tree Command - node pr.js tree <dirname>
+                      2) Organize Command- node pr.js organize <dirname>
+                      3) Help Command - node pr.js help`);
+  }
+
+  module.exports={
+      helpKey : helpfn
+
+  }
